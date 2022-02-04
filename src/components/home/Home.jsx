0cfdc6663cb2ae997fbbb160be68296bb7186464
Ranto Rabarisoa <ranto.rabarisoa@gmail.com>
2022-02-04 10:59:37 +0300

    home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import foot from '../../assets/foot.png';
+import tennis from '../../assets/tennis.png';
+import basket from '../../assets/basket.jpg';
+
 import './home.css'
 
 const Home = () => {
-  return <div>
-      coucou1
-  </div>;
+  return (
+    <div className="home">
+      <div className="hometext">
+        <h1>Welcome to SportAkaiky !</h1>
+      </div>
+      <div className="secondtitle">
+        <h3>
+        chose your path young padawan
+        </h3>
+      </div>
+       <div className="sport-type">
+         <div className="foothome">
+          <img className="footstyle" src={foot} alt="foot" />
+         </div>
+         <div className="tennishome">
+           <img className="tennisstyle" src={tennis} alt="tennis" />
+         </div>
+         <div className="baskethome">
+           <img className="basketstyle" src={basket} alt="basket" />
+        </div>
+      </div>
+    </div>
+
+);
 };
+
+
 
 export default Home;
